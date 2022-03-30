202d4bb14a061767f15d85952f1f6b446ffd3cdf
David Mitchell <Davidlee.Mitchell2@Gmail.com>
2022-03-30 15:17:59 -0500

    exercise solutions

--- tables_exercises.sql
+++ tables_exercises.sql
@@ -1,6 +1,17 @@
-use employees;
+use employees; 
 
 show tables;
+
+describe employees;
+
+
+
+# Which table(s) do you think contain a numeric type column? The emp_no table contain a numeric type column
+# Which table(s) do you think contain a string type column? The first, last and gender tables contain string type columns
+# Which table(s) do you think contain a date type column? The birth_date and hire_date would contain the date type column
+# What is the relationship between the employees and the departments tables? Both have default null values
+
+SHOW CREATE TABLE dept_manager;
 
 
 select * from employees limit 10;
