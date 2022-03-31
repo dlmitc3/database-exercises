16e9dd8cff31bbf75c1836ba75390b62af6d09e1
David Mitchell <Davidlee.Mitchell2@Gmail.com>
2022-03-31 14:06:24 -0500

    where_exercises

--- where_exercises.sql
+++ where_exercises.sql
@@ -1,1 +1,50 @@
-USE employees
+USE employees;
+# 2. Find all current or previous employees with first names 'Irena', 'Vidya', or 'Maya' using IN. 
+# Enter a comment with the number of records returned. 
+# Count 709
+
+SELECT COUNT(emp_no)
+FROM employees 
+WHERE first_name IN ('Irena', 'Vidya', 'Maya');
+
+# 3. Find all current or previous employees with first names 'Irena', 'Vidya', or 'Maya', as in Q2, but use OR instead of IN. 
+# Enter a comment with the number of records returned. Returned 709 records
+# Does it match number of rows from Q2? Yes
+
+SELECT COUNT(emp_no)
+FROM employees 
+WHERE first_name = 'Irena'
+OR first_name = 'Vidya'
+OR first_name = 'Maya';
+
+# 4. Find all current or previous employees with first names 'Irena', 'Vidya', or 'Maya', using OR, and who is male. 
+# Enter a comment with the number of records returned. returned 612 records
+
+SELECT 
+COUNT(emp_no) 
+FROM employees 
+WHERE gender = 'M'
+	AND first_name = 'Irena'
+	OR first_name = 'Vidya'
+	OR first_name = 'Maya';
+
+
+# 5. Find all current or previous employees whose last name starts with 'E'. Enter a comment with the number of employees whose last name starts with E.
+
+SELECT COUNT(last_name)
+FROM employees 
+WHERE last_name LIKE 'E%'
+
+# 6. Find all current or previous employees whose last name starts or ends with 'E'. Enter a comment with the number of employees whose last name starts or ends with E. How many employees have a last name that ends with E, but does not start with E?
+
+# 7. Find all current or previous employees employees whose last name starts and ends with 'E'. Enter a comment with the number of employees whose last name starts and ends with E. How many employees' last names end with E, regardless of whether they start with E?
+
+# 8. Find all current or previous employees hired in the 90s. Enter a comment with the number of employees returned.
+
+# 9. Find all current or previous employees born on Christmas. Enter a comment with the number of employees returned.
+
+# 10. Find all current or previous employees hired in the 90s and born on Christmas. Enter a comment with the number of employees returned.
+
+# 11. Find all current or previous employees with a 'q' in their last name. Enter a comment with the number of records returned.
+
+# 12. Find all current or previous employees with a 'q' in their last name but not 'qu'. How many employees are found?
